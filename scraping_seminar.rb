e5982294a2aad667ff320f9325bcdb43f76ccdcd
BanMasanobu <babababandits@gmail.com>
2015-11-22 12:36:27 +0900

    use db

--- scraping_seminar.rb
+++ scraping_seminar.rb
@@ -1,24 +1,27 @@
 require 'open-uri'
 require 'nokogiri'
 require 'robotex'
+require 'sqlite3'
 
 robotex = Robotex.new
 p robotex.allowed?("https://www.goodfind.jp/2016/seminar/")
 
+db = SQLite3::Database.new('scraping.db')
+db.execute('CREATE TABLE IF NOT EXISTS seminars (title varchar(100), sub_title varchar(200));')
+
 url = "https://www.goodfind.jp/2016/seminar/"
 user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36'
 
-open('output_seminar.txt', 'w') do |file|
-  charset = nil
-  html = open(url, "User-Agent" => user_agent) do |f|
-    charset = f.charset
-    f.read
-  end
+charset = nil
+html = open(url, "User-Agent" => user_agent) do |f|
+  charset = f.charset
+  f.read
+end
 
-  doc = Nokogiri::HTML.parse(html, nil, charset)
+doc = Nokogiri::HTML.parse(html, nil, charset)
 
-  doc.css("#toggle_view_section > div.toggle.list_view > div > div > ul > li > a > div.seminar_desc > h3").each do |title|
-    puts title.text
-    file.write "#{title.text},\n"
-  end
+doc.css("#toggle_view_section > div.toggle.list_view > div > div > ul > li > a > div.seminar_desc").each do |title|
+  puts title.css("h3").text
+  puts title.css("p").text
+  db.execute 'INSERT INTO seminars values ( ?, ? );', ["#{title.css("h3").text}", "#{title.css("p").text}"]
 end
--- view_seminar.rb
+++ view_seminar.rb
@@ -1,17 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
 
 get '/view_seminar' do
 
+  db = SQLite3::Database.new('scraping.db')
+  db.results_as_hash = true
+
   html = '<h1>goodfindセミナー一覧</h1>'
 
-  html << "<ul>"
-  open('output_seminar.txt', 'r') do |file|
-    file.each do |line|
-      html << "<li>#{line}<br></li>"
-    end
+  html << "<dl>"
+  db.execute('SELECT * FROM seminars;') do |row|
+    puts row
+    html << "<dt>#{row["title"]}</dt>"
+    html << "<dd>#{row["sub_title"]}</dd>"
   end
-  html << "</ul>"
+  html << "</dl>"
 
   return html
 end
